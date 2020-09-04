c9f42b09a35e355ca6d319cca3b4d0bcc638c3ad
Sushama Sivananthan <70018868+sushamasivananthan@users.noreply.github.com>
2020-09-04 16:41:02 +0200

    Update octal to string.py

--- octal to string.py
+++ octal to string.py
@@ -12,7 +12,7 @@ def octal_to_string(octal):
                 result+="-"
     return result
     
-print(octal_to_string(755)) # Should be rwxr-xr-x
-print(octal_to_string(644)) # Should be rw-r--r--
-print(octal_to_string(750)) # Should be rwxr-x---
-print(octal_to_string(600)) # Should be rw-------
+print(octal_to_string(755)) 
+print(octal_to_string(644)) 
+print(octal_to_string(750)) 
+print(octal_to_string(600)) 
